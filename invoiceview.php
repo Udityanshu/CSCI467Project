b1cd6f1979851c9084b22d5623937d62d9f8dddc
cbhatia19 <58056976+cbhatia19@users.noreply.github.com>
2020-04-29 23:18:37 -0500

    Update invoiceview.php

--- invoiceview.php
+++ invoiceview.php
@@ -5,50 +5,46 @@
     </title>
   </head>
 <center>
-<body bgcolor='FFA07A'>	// test 
+<body bgcolor='FFA07A'>	<!-- set the background color as light salmon to match theme --> 
 <h1> Invoice View Please Enter An Order ID to Retrieve Information</h1>
   <?php
-    require("connection.php"); //using conn.php file
+    require("connection.php"); // this file uses data from database (info in connection.php file)
 
-    //try adding form to select order id:
-    echo '<form action="invoiceview.php" method="post">'; //resubmiting back to invoice.php
-      echo 'Order ID: <input type=text name=orderid id=orderid><br>'; //input box for order id number
-      echo '<br><input type="submit" name="submit" value="Submit"><br>'; //submit button
+    
+    echo '<form action="invoiceview.php" method="post">'; // submit data to invoiceview.php using POST method
+      echo 'Order ID: <input type=text name=orderid id=orderid><br>'; // field to input order id number
+      echo '<br><input type="submit" name="submit" value="Submit"><br>'; // Button for worker to submit order number
     echo '</form>';
 
 echo "<h2> Order Details:</h2>";
-    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    if($_SERVER['REQUEST_METHOD'] == 'POST')	// check if data is being sent using POST method
     {
-        $orderid = $_POST['orderid'];
-        // 
-	// The select staement to show items ordered for the ID given
-        $statement= $pdo1->query('select * from ordereditems where orderID = '.$orderid);
-        echo '<table border=5, cellspacing=5, cell padding=5>';
-	echo '<tbody style="background-color:#FF726f">';
-	//table settings and colors
+        $orderid = $_POST['orderid'];		// set order id to its equivalent
+        $statement= $pdo1->query('select * from ordereditems where orderID = '.$orderid);  // pull item from DB where ID matches 
+        echo '<table border=5, cellspacing=5, cell padding=5>';		// table and cell formatting
+	echo '<tbody style="background-color:#FF726f">';		// table cell color set to peach red
 
-        echo '<th>Order ID</th>';
-        echo '<th>Product Name</th>';
+        echo '<th>Order ID</th>';					// sets the column headers of the table to
+        echo '<th>Product Name</th>';					// these titles
         echo '<th>Unit Price ($)</th>';
         echo '<th>Amount On Hand</th>';
-
-        //displaying the information from the query in the table
-        while($row = $statement->fetch(PDO::FETCH_ASSOC))
+	    
+        while($row = $statement->fetch(PDO::FETCH_ASSOC))		// fetch information for query in the table
         {
           $productid = $row['productID'];
           $stmtgetdescription = $pdo2->query('select * from parts where number = '.$productid);
-          // 'select quantity from orderedItems where orderID'
+          // display the part from DB with the appropriate order ID
 
           while($description = $stmtgetdescription->fetch(PDO::FETCH_ASSOC))
           {
             echo '<tr><td>';
-            echo $row['orderID']; //order id num
+            echo $row['orderID']; // display the order ID into the table
             echo '</td><td> ';
-            echo $description['description']; //prodcut name
+            echo $description['description']; // put items description into the column for description
             echo '</td><td> ';
-            echo $description['price']; //price of products
+            echo $description['price']; // display the unit price in $
             echo '</td><td> ';
-            echo $row['quantity']; //amount in store cna be changed in admin view
+            echo $row['quantity']; // how much of this item is available in stock is displayed here
             echo '</td></tr>';
           }
 
@@ -56,31 +52,29 @@ echo "<h2> Order Details:</h2>";
 
         echo '</table>';
 	echo "<br><br>";
-	echo "<h2>Total Order Stats:</h2>";
+	echo "<h2>Total Order Stats:</h2>";	// display below the total price and shipping fees for the order
 
 
 
-//Total Statistics for total order information here in this table
-        $statement2 = $pdo1->query('select * from orders where ordersID = '.$orderid);
-        while($row2 = $statement2->fetch(PDO::FETCH_ASSOC))
+        $statement2 = $pdo1->query('select * from orders where ordersID = '.$orderid);	// display the part from DB with the appropriate order ID
+        while($row2 = $statement2->fetch(PDO::FETCH_ASSOC))	// fetch information for query in the table
         {
-            echo '<table border=5,cellspacing=5,cellpadding=5>';
-	    echo '<tbody style="background-color:#FF726f">';
-		//settings for the table color and size eetc
+            echo '<table border=5,cellspacing=5,cellpadding=5>';	// table and cell formatting
+	    echo '<tbody style="background-color:#FF726f">';		// table cell color set to peach red
 
-            echo '<tr><th>Net Price Of Products ($)</th><td>';
-            echo $row2['totalprice'];
+            echo '<tr><th>Net Price Of Products ($)</th><td>';		// sets the column headers of the table to these titles
+            echo $row2['totalprice'];					// displays the net price of the order
             echo '</td></tr>';
 
-            echo '<tr><th>Shipping and Handling Fees</th><td>';
-            echo $row2['addfees'];
+            echo '<tr><th>Shipping and Handling Fees</th><td>';		// sets the column headers of the table to these titles
+            echo $row2['addfees'];					// displays the shipping/handling fee amounts
             echo '</td></tr>';
 
-            echo '<tr><th>Total Price ($)</th><td>';
-            echo $row2['finalprice'];
+            echo '<tr><th>Total Price ($)</th><td>';			// sets the column headers of the table to these titles
+            echo $row2['finalprice'];					// displays the total price of order (net + shipping)
             echo '</td></tr>';
 
-            echo '</table>';
+            echo '</table>';						// end table
         }
 
     }
@@ -93,7 +87,7 @@ echo "<h2> Order Details:</h2>";
 <br><br><br><br><br><br><br><br><br><br><br><br>
 Thank You For Shopping With Us
 <br>
-For more information during COVID-19 Please Contact US!
+For more information during COVID-19 Please Contact US!			<!-- footer -->
 <br>
 CSCI 467 - Group 1A
 
