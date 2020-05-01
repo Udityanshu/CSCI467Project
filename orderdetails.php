985da1ece3daaac6ec09a59817b6a59396b968f5
cbhatia19 <58056976+cbhatia19@users.noreply.github.com>
2020-04-30 20:52:17 -0500

    Update orderdetails.php

--- orderdetails.php
+++ orderdetails.php
@@ -4,33 +4,33 @@
   <?php
     try
     {
-      $dsn1 = "mysql:host=courses;dbname=z1853066";
-      include("pswrds.php");
-      $pdo1 = new PDO($dsn1, $username1, $password1);
+      $dsn1 = "mysql:host=courses;dbname=z1853066";           // Connect to database on MariaDB using this login credentials
+      include("pswrds.php");                                  // include pswrds.php to match the given password                                    
+      $pdo1 = new PDO($dsn1, $username1, $password1);         // use credentials username1 and password1 to establish connection
     }
 
     catch(PDOexception $exception1)
     {
-      echo "Database connection failed: " . $exception1->getMessage();
+      echo "Database connection failed: " . $exception1->getMessage();        // print this error message if connection is unsuccessful
     }
 
     try
     {
-      $connection2 = "mysql:host=blitz.cs.niu.edu;dbname=csci467";
-      include("pswrds.php");
-      $pdo2 = new PDO($connection2, $username2, $password2);
+      $connection2 = "mysql:host=blitz.cs.niu.edu;dbname=csci467";            // connect to blitz.cs.niu.edu for legacy DB
+      include("pswrds.php");                                                  // include pswrds.php to match the given password
+      $pdo2 = new PDO($connection2, $username2, $password2);                  // use credentials username2 and password2 to establish connection
     }
 
     catch(PDOexception $exception2)
     {
-      echo "Database connection failed: " . $exception2->getMessage();
+      echo "Database connection failed: " . $exception2->getMessage();        // print this error message if connection is unsuccessful
     }
 
-    $sql1 = "SELECT ordersID, custid, finalprice, date, status FROM orders";
+    $sql1 = "SELECT ordersID, custid, finalprice, date, status FROM orders";  // order ID, custID, final price, date of purcase and status of order will show from orders table in database
     if (array_key_exists('sql', $_REQUEST))
     {
       $sql1 = "";
-      $asarray = unserialize(base64_decode($_REQUEST['sql']));
+      $asarray = unserialize(base64_decode($_REQUEST['sql']));                // unserialize value from array
 
       foreach($asarray as $word)
       {
@@ -42,33 +42,33 @@
 
     $toarray = explode(" ", $sql1);
 
-    echo "<form method=post action=http://students.cs.niu.edu/~z1853066/CSCI467/Main/U/orders.php>";
-      $a2s= base64_encode(serialize($toarray));
+    echo "<form method=post action=http://students.cs.niu.edu/~z1853066/CSCI467/Main/U/orders.php>";    // submit data to orders.php using POST method
+      $a2s= base64_encode(serialize($toarray));     // re-serialize items to an array
       echo "<input type=hidden name='sql'
                    value=$a2s1/>";
-      echo "<input type=hidden name='viewall'
+      echo "<input type=hidden name='viewall'       // option for user of this interface to view all customer orders
                    value='View All Orders'/>";
-      echo "<input type=hidden name='search'
+      echo "<input type=hidden name='search'        // option for user of this interface to search for customer orders
                    value='R'/>";
       echo "<input type=submit name='orders'
-                   value='Return to Search'/>";
+                   value='Return to Search'/>";     // to return to search field
     echo "</form>";
 
     $orderid = 0;
-    if (array_key_exists('oid', $_REQUEST))
+    if (array_key_exists('oid', $_REQUEST))         // if requested for a valid order id
     {
       $orderid = intval($_REQUEST['oid']);
     }
 
-    $sqlord = "SELECT * FROM orders WHERE ordersID = $orderid";
+    $sqlord = "SELECT * FROM orders WHERE ordersID = $orderid";       // select the requested order id from database
     $queryord = $pdo1->query($sqlord);
-    $arrayord = $queryord->fetchAll(PDO::FETCH_ASSOC);
+    $arrayord = $queryord->fetchAll(PDO::FETCH_ASSOC);                // fetch all orders with this id
 
     $order = $arrayord[0];
-    $customer = $arrayord[0]['custid'];
+    $customer = $arrayord[0]['custid'];                               // pull up customer with this id
 
-    echo "<h3>Order Details</h3>";
-    echo "<table border=3>";
+    echo "<h3>Order Details</h3>";                                    // These specify the column headers for table "Order Details"
+    echo "<table border=3>";                                          // table border formatting
       echo "<tr>";
         echo "<th>Order ID</th>";
         echo "<th>Customer ID</th>";
@@ -84,20 +84,20 @@
         foreach($order as $data)
         {
           echo "<td>";
-            echo "$data";
+            echo "$data";           // display the appropriate data in the table cell
           echo "</td>";
         }
       echo "</tr>";
-    echo "</table>";
+    echo "</table>";                // end of table
 
    echo "<h3>Item Details</h3>";
-   $sqlprod = "SELECT productID, quantity FROM ordereditems
+   $sqlprod = "SELECT productID, quantity FROM ordereditems       // select the requested product id and availble quantity from database for this order ID
               WHERE orderID = $orderid";
    $queryprod = $pdo1->query($sqlprod);
-   $arrayprod = $queryprod->fetchAll(PDO::FETCH_ASSOC);
+   $arrayprod = $queryprod->fetchAll(PDO::FETCH_ASSOC);           // fetch info for all orders with this id
 
-   echo "<table border=3>";
-     echo "<tr>";
+   echo "<table border=3>";                                       // table border formatting
+     echo "<tr>";                                                 // These specify the column headers for table "Item Details"
        echo "<th>Product Number</th>";
        echo "<th>Product Description</th>";
        echo "<th>Quantity Ordered</th>";
@@ -107,59 +107,59 @@
        echo "<th>Added Weight</th>";
      echo "</tr>";
 
-    foreach($arrayprod as $ordereditem)
+    foreach($arrayprod as $ordereditem)                           // this loop for each order item
     {
-      $databdesc= "SELECT description, price, weight FROM parts WHERE number = $ordereditem[productID]";
+      $databdesc= "SELECT description, price, weight FROM parts WHERE number = $ordereditem[productID]";  // select data for product with this ID
       $querydesc= $pdo2->query($databdesc);
       $arraydesc = $querydesc ->fetchAll(PDO::FETCH_ASSOC);
-      $description = $arraydesc[0]['description'];
+      $description = $arraydesc[0]['description'];                // add data: description, weight and price for product with this ID
       $weight = $arraydesc[0]['weight'];
       $price = $arraydesc[0]['price'];
 
       echo "<tr>";
         echo "<td>";
-          echo "$ordereditem[productID]";
+          echo "$ordereditem[productID]";                         // display product ID in cell
         echo "</td>";
 
         echo "<td>";
-          echo "$description";
+          echo "$description";                                    // display item description
         echo "</td>";
 
         echo "<td>";
-          echo "$ordereditem[quantity]";
+          echo "$ordereditem[quantity]";                          // display quantity that user has requested
         echo "</td>";
 
         echo "<td>";
-          echo "$price";
+          echo "$price";                                          // display item's price
         echo "</td>";
 
-        $trueprice = ($ordereditem['quantity'] * $price);
+        $trueprice = ($ordereditem['quantity'] * $price);         // multiply unit price for 1 item with the quantity ordered...
         echo "<td>";
-          echo "$trueprice";
+          echo "$trueprice";                                      // ... to get actual price
         echo "</td>";
 
         echo "<td>";
           echo "$weight";
         echo "</td>";
 
-        $trueweight = ($ordereditem['quantity'] * $weight);
+        $trueweight = ($ordereditem['quantity'] * $weight);       // multiply unit weight for 1 item with the quantity ordered...
         echo "<td>";
-          echo "$trueweight";
+          echo "$trueweight";                                     // ... to get the actual weight for all products in order
         echo "</td>";
       echo "</tr>";
     }
 
-   echo "</table>";
+   echo "</table>";                                               // end of table
 
 
    echo "<h3>Customer Details</h3>";
 
-   $internetcustomer = "SELECT name, email, address, ccnum FROM customer WHERE customerID = $customer";
+   $internetcustomer = "SELECT name, email, address, ccnum FROM customer WHERE customerID = $customer";   // select customer info from database for viewing on page
    $querycust = $pdo1->query($internetcustomer);
    $arraycust = $querycust->fetchAll(PDO::FETCH_ASSOC);
 
    echo "<table border=3>";
-     echo "<tr>";
+     echo "<tr>";                                             // These specify the column headers for table "Customer Details"
        echo "<th>Customer Name</th>";
        echo "<th>Customer Email</th>";
        echo "<th>Customer Address</th>";
@@ -167,18 +167,18 @@
      echo "</tr>";
 
      echo "<tr>";
-       foreach($arraycust as $cust)
+       foreach($arraycust as $cust)                           // For each customer, display the following info...
        {
          echo "<td>";
-           echo "$cust[name]";
+           echo "$cust[name]";                                // customers name
          echo "</td>";
 
          echo "<td>";
-           echo "$cust[email]";
+           echo "$cust[email]";                               // customers email id
          echo "</td>";
 
          echo "<td>";
-           echo "$cust[address]";
+           echo "$cust[address]";                             // customers given mailing address
          echo "</td>";
 
          $star = "************";
@@ -188,7 +188,7 @@
          echo "</td>";
        }
      echo "</tr>";
-   echo "</table>";
+   echo "</table>";                                           // end of table
   ?>
 </html>
 
